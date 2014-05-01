b5afe5a31c4ddf054818512a3a52750b8a9b96b5
Michael Williams <dinosaur@riseup.net>
2014-05-01 00:55:16 -0700

    input editing works

--- client/js/list.js
+++ client/js/list.js
@@ -3,5 +3,14 @@ Template.list.events({
     List.insert({
       name: "new",
     })
-  }
-})
+  },
+  'input .item': function (e) {
+    console.log(e);
+    var id = e.currentTarget.dataset.id;
+    List.update({
+      _id: id,
+    }, {
+      name: e.currentTarget.value
+    });
+  },
+});
