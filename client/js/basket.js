e32199d62a7a521966c49652d390a535f9ba9494
Derek Razo <derekrazo@gmail.com>
2014-06-28 23:59:59 -0700

    allow users to add items to baskets with url to image

--- client/js/basket.js
+++ client/js/basket.js
@@ -3,6 +3,22 @@ Template.basket.events({
     var id = e.currentTarget.dataset.id;
     Router.go('basket_item', { id: id });
   },
+
+  	'click #add_basket_item' : function(e) {
+    	//alert('yo')
+	    var item_name = $('#add_item_name').val();
+	    var item_url = $('#add_item_image_url').val();
+	    
+	    //do validation on form={firstname:'first name', lastname: 'last name', email: 'email@email.com'}
+	    Basket_items.insert({
+	      name: item_name,
+	      image_url: item_url,
+	      basket: "1",
+	      started_by: "Michael Williams",
+	    })
+
+    	e.preventDefault(); //prevent page refresh
+	},
 });
 
 
